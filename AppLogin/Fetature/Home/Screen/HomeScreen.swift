9c2890cca41b2417094e1c20685bbc474bbd14f1
Code Marcos <marcosv.code@gmail.com>
2024-12-14 18:35:27 -0300

    Add Constraints for Elements

---
+++ AppLogin/Fetature/Home/Screen/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  AppLogin
+//
+//  Created by Marcos Vinícius Vieira on 14/12/24.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
