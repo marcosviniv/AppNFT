1c486323e654a2729a3073acd17e0293281aa687
Code Marcos <marcosv.code@gmail.com>
2024-12-15 01:41:32 -0300

    Exibindo célula de filtro na Home.

---
+++ AppLogin/Fetature/Cells/NftFilter/NftFilterCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  NftFilterCollectionViewCell.swift
+//  AppLogin
+//
+//  Created by Marcos Vinícius Vieira on 14/12/24.
+//
+
+import UIKit
+
+class NftFilterCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ AppLogin/Fetature/Cells/NftFilter/Screen/NftFilterCollectionViewCellScreen.swift
@@ -0,0 +1,20 @@
+//
+//  NftFilterCollectionViewCellScreen.swift
+//  AppLogin
+//
+//  Created by Marcos Vinícius Vieira on 14/12/24.
+//
+
+import UIKit
+
+class NftFilterCollectionViewCellScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
