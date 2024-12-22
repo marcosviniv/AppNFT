ed958578aa4593bb49cd2287d71ce4119df475b3
Code Marcos <marcosv.code@gmail.com>
2024-12-22 01:03:17 -0300

    Exibindo primeira célula de detalhes.

---
+++ AppLogin/Fetature/Cells/NftImageTableViewCell/NftImageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NftImageTableViewCell.swift
+//  AppLogin
+//
+//  Created by Marcos Vinícius Vieira on 22/12/24.
+//
+
+import UIKit
+
+class NftImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ AppLogin/Fetature/Cells/NftImageTableViewCell/Screen/NftImageTableViewCellScreen.swift
@@ -0,0 +1,20 @@
+//
+//  NftImageTableViewCellScreen.swift
+//  AppLogin
+//
+//  Created by Marcos Vinícius Vieira on 22/12/24.
+//
+
+import UIKit
+
+class NftImageTableViewCellScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
