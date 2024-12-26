26af5fff175cf338504938d29005ebfe62780819
Code Marcos <marcosv.code@gmail.com>
2024-12-26 00:23:35 -0300

    Finalizando LatestDealsScreen

---
+++ AppLogin/Fetature/Cells/ListOffersTableViewCell/Screen/ListOffersTableViewCellScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ListOffersTableViewCellScreen.swift
+//  AppLogin
+//
+//  Created by Marcos Vinícius Vieira on 25/12/24.
+//
+
+import UIKit
+
+class ListOffersTableViewCellScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AppLogin/Utils/Extension/UIView+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Extension.swift
+//  AppLogin
+//
+//  Created by Marcos Vinícius Vieira on 25/12/24.
+//
+
+import Foundation
