231dc076d252f301810198d223ac60b4f070a225
Code Marcos <marcosv.code@gmail.com>
2024-12-28 18:01:04 -0300

    Finalizando AppNFT.

---
+++ AppLogin/Fetature/Cells/ProfileTableViewCell/ProfileTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileTableViewCell.swift
+//  AppLogin
+//
+//  Created by Marcos Vinícius Vieira on 28/12/24.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ AppLogin/Fetature/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewModel.swift
+//  AppLogin
+//
+//  Created by Marcos Vinícius Vieira on 28/12/24.
+//
+
+import UIKit
+
+class ProfileViewModel: NSObject {
+
+}
